e4feaea02592369227e19e0648c5aec106f67821
Ryan Troy Lattibeaudiere <r_latti@hotmail.com>
2019-10-30 21:29:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,32 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  nums = ["ryan", "kevin", "barry", "stan"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+ array << element
 end
 
 def add_element_to_start_of_array(array, element)
   
+  array.unshift(element)
+  
 end
 
 def remove_element_from_end_of_array(array)
+  element = array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
-  
+  element = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
